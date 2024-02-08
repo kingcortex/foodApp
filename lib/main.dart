4ca9e6380d964ae4c2031111231d5da2a37b510d
kingCortex <souleydiom225@gmail.com>
2024-02-08 16:12:24 +0000

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:app_cooking/model/my_shared_preferences.dart';
 import 'package:app_cooking/provider/home_filter_provider.dart';
 import 'package:app_cooking/router/app_router.dart';
 import 'package:app_cooking/screens/can.dart';
-import 'package:app_cooking/screens/home_page.dart';
+//import 'package:app_cooking/screens/home_page.dart';
 import 'package:app_cooking/screens/profile_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
